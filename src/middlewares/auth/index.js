baabdae975561650f760766c0acc7c8587134df4
bouba_king <boubasambare@yahoo.it>
2020-02-12 23:04:47 +0100

    add jwt

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -1,10 +1,11 @@
 const basicAuth = require('express-basic-auth')
-const User = require('../../services/auth')
+const User = require('../../services/models/auth')
 const atob = require('atob')
 
-const checkUser = async (username, password, cb) => {
-    const {user} = await User.authenticate()(username,password)
-    return cb(null, user)
+    checkUser = async (username, password, next) => {
+        const {user} =  await User.authenticate()(username,password)
+         next(null, user)
+   
 }
 
 module.exports = {
